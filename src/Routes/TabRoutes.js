e7704b1f8fdc9514295a7f7d43be695d7addf013
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-20 15:53:15 -0300

    Add Icons And Custom Tab Bar

--- src/Routes/TabRoutes.js
+++ src/Routes/TabRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo, FontAwesome, Octicons, MaterialIcons } from '@expo/vector-icons';
 
 import HomeScreen from '../Screens/Home';
 import LearnScreen from '../Screens/Learn';
@@ -13,12 +14,51 @@ export default function TabRoutes(){
     return(
         <NavigationContainer independent={ true }>
 
-            <Navigator>
+            <Navigator tabBarOptions={{
+                style:{
+                    backgroundColor:'#17324f',
+                    borderTopColor:'transparent'
+                },
+                activeTintColor:'#91c704',
+                inactiveTintColor:'#fff',
+            }}>
 
-                <Screen name="Home" component={ HomeScreen } />
-                <Screen name="Learn" component={ LearnScreen } />
-                <Screen name="Progress" component={ ProgressScreen } />
-                <Screen name="Profile" component={ ProfileScreen } />
+                <Screen 
+                    name="Home" 
+                    component={ HomeScreen }
+                    options={{
+                        tabBarIcon: ({ color }) => (
+                            <Entypo name="home" size={ 23 } color={ color } />
+                        )
+                    }} 
+                />
+                <Screen 
+                    name="Learn" 
+                    component={ LearnScreen }
+                    options={{
+                        tabBarIcon: ({ size, color }) => (
+                            <FontAwesome name="book" size={ 23 } color={ color } />
+                        )
+                    }} 
+                />
+                <Screen 
+                    name="Progress" 
+                    component={ ProgressScreen }
+                    options={{
+                        tabBarIcon: ({ size, color }) => (
+                            <Octicons name="graph" size={ 23 } color={ color } />
+                        )
+                    }} 
+                />
+                <Screen 
+                    name="Profile" 
+                    component={ ProfileScreen }
+                    options={{
+                        tabBarIcon: ({ size, color }) => (
+                            <MaterialIcons name="person" size={ 23 } color={ color } />
+                        )
+                    }} 
+                />
 
             </Navigator>
 
