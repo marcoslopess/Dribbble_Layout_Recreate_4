6631aae15a586606e43dff25386c03bf18d7032a
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-20 15:39:46 -0300

    Create Others Screen Components

---
+++ src/Screens/Progress/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+
+export default function ProgressScreen(){
+    return(
+        <Container>
+            <Title>Here Go Progress Screen</Title>
+        </Container>
+    )
+}
---
+++ src/Screens/Progress/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    background-color:#112337;
+    flex:1;
+    align-items:center;
+    justify-content:center;
+`;
+
+export const Title = styled.Text``;
