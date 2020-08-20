070eac8d19f22216f59d61812d82a8fcd3a24f10
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-20 15:16:16 -0300

    Conclused Form Screen

--- src/Screens/Form/index.js
+++ src/Screens/Form/index.js
@@ -1,11 +1,106 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { FontAwesome, MaterialIcons, Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container, Title } from './styles';
+import Exercise from '../../Images/Exercise.png';
+
+import {Container, 
+        Title, 
+        ExerciseImg, 
+        Form, 
+        Label,
+        InputContainer, 
+        Input,
+        ShowPasswordButton,
+        ErrorMessage,
+        SubmitForm,
+        TextSubmitForm, 
+} from './styles';
 
 export default function FormScreen(){
+    const [ isPasswordHidden, setIsPasswordHidden ] = useState(true)
+
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const [ errorMessage, setErrorMessage ] = useState('');
+
+    const { navigate } = useNavigation();
+
+    function handlePasswordVisibility(){
+        setIsPasswordHidden(!isPasswordHidden)
+    }
+
+    function handleSubmitForm(){
+        if( name === '' || email === '' || password === '' ){
+            setErrorMessage('Please Fill In All The Fields!');
+        }else{
+            navigate('Others');
+        }
+    }
+
     return(
         <Container>
-            <Title>Here Go Form Screen</Title>
+
+            <Title>Welcome to MyouMus!</Title>
+
+            <ExerciseImg source={ Exercise } />
+
+            <Form>
+
+                <Label>Username:</Label>
+
+                <InputContainer>
+
+                    <Input 
+                        placeholder="Enter Your Username"
+                        onChangeText={ text => setName(text) }
+                    />
+                    <FontAwesome name="user" size={ 23 } color="#d3d3d3" />
+                    
+                </InputContainer>
+
+                <Label>Email:</Label>
+
+                <InputContainer>
+
+                    <Input 
+                        placeholder="Enter Your Email"
+                        onChangeText={ text => setEmail(text) }
+                    />
+                    <MaterialIcons name="email" size={ 23 } color="#d3d3d3" />
+
+                </InputContainer>
+
+                <Label>Password:</Label>
+
+                <InputContainer>
+
+                    <Input 
+                        placeholder="Enter Your Password"
+                        onChangeText={ text => setPassword(text) } 
+                        secureTextEntry={ isPasswordHidden } 
+                    />
+
+                    <ShowPasswordButton onPress={ handlePasswordVisibility }>
+
+                        { isPasswordHidden === true 
+                        ? <Feather name="eye" size={ 23 } color="#d3d3d3" />
+                        : <Feather name="eye-off" size={ 23 } color="#d3d3d3" /> }
+
+                    </ShowPasswordButton>
+
+                </InputContainer>
+                
+                <ErrorMessage>{errorMessage}</ErrorMessage>
+
+                <SubmitForm onPress={ handleSubmitForm }>
+                    <TextSubmitForm>Enter</TextSubmitForm>
+                </SubmitForm>
+
+            </Form>
+
         </Container>
     )
 }
--- src/Screens/Form/styles.js
+++ src/Screens/Form/styles.js
@@ -1,9 +1,71 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+    background-color:#112337;
     flex:1;
     align-items:center;
     justify-content:center;
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+    color:#fff;
+    font-weight:bold;
+    font-size:23px;
+    margin-top:60px
+`;
+
+export const ExerciseImg = styled.Image`
+    width:200px;
+    height:200px;
+    margin-top:50px;
+`;
+
+export const Form = styled.KeyboardAvoidingView`
+    width:80%;
+`;
+
+export const Label = styled.Text`
+    color:#456E84;
+    font-weight:bold;
+    margin-top:25px;
+`;
+
+export const InputContainer = styled.View`
+    flex-direction:row;
+    align-items:center;
+    justify-content:space-between;
+    border-bottom-color:#d3d3d3;
+    border-bottom-width:2px;
+`;
+
+export const Input = styled.TextInput`
+    color:#fff;
+    width:90%;
+    padding:5px;
+    margin-top:10px;
+`;
+
+export const ShowPasswordButton = styled.TouchableOpacity``;
+
+export const ErrorMessage = styled.Text`
+    color:#ff1943;
+    font-weight:bold;
+    margin-top:20px;
+`;
+
+export const SubmitForm = styled.TouchableOpacity`
+    background-color:#91c704;
+    width:100%;
+    height:50px;
+    border-radius:10px;
+    margin-top:15px;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+`;
+
+export const TextSubmitForm = styled.Text`
+    color:#fff;
+    font-weight:bold;
+    font-size:16px;
+`;
