6feb221ab066e0ee2de3845c4dc24d41313ae943
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-20 15:42:21 -0300

    Imported New Screen Components in TabRoutes

--- src/Routes/TabRoutes.js
+++ src/Routes/TabRoutes.js
@@ -4,6 +4,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../Screens/Home';
 import LearnScreen from '../Screens/Learn';
+import ProgressScreen from '../Screens/Progress';
+import ProfileScreen from '../Screens/Profile';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -15,6 +17,8 @@ export default function TabRoutes(){
 
                 <Screen name="Home" component={ HomeScreen } />
                 <Screen name="Learn" component={ LearnScreen } />
+                <Screen name="Progress" component={ ProgressScreen } />
+                <Screen name="Profile" component={ ProfileScreen } />
 
             </Navigator>
 
