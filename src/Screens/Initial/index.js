574e98e3792374fa3dc3bdf62273f15df115b9e0
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-20 12:19:40 -0300

    Create a Screens Components for Add in Routes

---
+++ src/Screens/Initial/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export default function InitialScreen(){
+    return(
+        <Container>
+            <Title>Here Go Initial Screen</Title>
+        </Container>
+    )
+}
---
+++ src/Screens/Learn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export default function LearnScreen(){
+    return(
+        <Container>
+            <Title>Here Go Learn Screen</Title>
+        </Container>
+    )
+}
