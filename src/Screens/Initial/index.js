19c16c799445b08768c9e9ae68ac09c6e8c44e79
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-20 13:08:36 -0300

    Finished Initial Screen

--- src/Screens/Initial/index.js
+++ src/Screens/Initial/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container, Title } from './styles';
+import Exercise from '../../Images/Exercise.png';
+
+import { Container, 
+        TopContainer, 
+        ExerciseImage,
+        BottomContainer,
+        WelcomeMessage,
+        InfoMessage,
+        ButtonLogin,
+        ButtonRegister,
+        TextButtons,
+} from './styles';
 
 export default function InitialScreen(){
+    const { navigate } = useNavigation();
+
+    function handleNavigationToTheFormScreen(){
+        navigate('Form');
+    }
+
     return(
         <Container>
-            <Title>Here Go Initial Screen</Title>
+          
+            <TopContainer>
+                
+                <ExerciseImage source={ Exercise } />
+
+            </TopContainer>
+
+            <BottomContainer>
+
+                <WelcomeMessage>Welcome to MyoMus!</WelcomeMessage>
+
+                <InfoMessage>Hope you are well today</InfoMessage>
+
+                <ButtonLogin onPress={ handleNavigationToTheFormScreen }>
+                    <TextButtons>Login</TextButtons>
+                    <AntDesign name="login" size={ 23 } color="#fff" />
+                </ButtonLogin>
+
+                <ButtonRegister onPress={ handleNavigationToTheFormScreen }>
+                    <TextButtons>Register</TextButtons>
+                    <MaterialIcons name="email" size={ 23 } color="#fff" />
+                </ButtonRegister>
+
+            </BottomContainer>
+
         </Container>
     )
 }
--- src/Screens/Initial/styles.js
+++ src/Screens/Initial/styles.js
@@ -1,10 +1,75 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-    background-color:#112337;
     flex:1;
+    background-color:#112337;
+`;
+
+export const TopContainer = styled.View`
+    background-color:#fff;
+    width:100%;
+    height:50%;
+    position:absolute;
+    top:0;
+    border-bottom-left-radius:70px;
+    border-bottom-right-radius:70px;
     align-items:center;
     justify-content:center;
 `;
 
-export const Title = styled.Text``;
+export const ExerciseImage = styled.Image`
+    width:200px;
+    height:200px;
+    margin-top:25px;
+`;
+
+export const BottomContainer = styled.View`
+    width:100%;
+    height:50%;
+    position:absolute;
+    bottom:0;
+    align-items:center;
+    justify-content:center;
+`;
+
+export const WelcomeMessage = styled.Text`
+    color:#fff;
+    font-weight:bold;
+    font-size:25px;
+`;
+
+export const InfoMessage = styled.Text`
+    color:rgba(255, 255, 255, 0.5);
+    font-size:16px;
+    margin-top:20px;
+    font-weight:bold;
+`;
+
+export const ButtonLogin = styled.TouchableOpacity`
+    background-color:#1b4069;
+    width:80%;
+    height:50px;
+    border-radius:10px;
+    margin-top:70px;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+`;
+
+export const ButtonRegister = styled.TouchableOpacity`
+    background-color:#91c704;
+    width:80%;
+    height:50px;
+    border-radius:10px;
+    margin-top:25px;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+`;
+
+export const TextButtons = styled.Text`
+    color:#fff;
+    font-weight:bold;
+    font-size:16px;
+    width:70%;
+`;
